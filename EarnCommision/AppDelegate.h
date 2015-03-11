bf1c63c9644d9ffa4aa8e403e94e2b82d9abbb9a
EvanZ <ivanz1234@163.com>
2015-03-12 01:24:27 +0800

    Initial Commit

---
+++ EarnCommision/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EarnCommision
+//
+//  Created by EvanZ on 15/3/12.
+//  Copyright (c) 2015年 EC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
